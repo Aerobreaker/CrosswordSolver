d81b3d4426a91d935aba86a7fe8d7abc1f7ed2cb
Aerobreaker <44306739+Aerobreaker@users.noreply.github.com>
2019-02-18 18:53:59 -0700

    Update crossword.py

--- crossword.py
+++ crossword.py
@@ -1,190 +1,11 @@
 from enum import auto, IntEnum, IntFlag
 
 
-#Example usage:
-#  PS C:\Python> py
-#  Python 3.6.6 (v3.6.6:4cf1f54eb7, Jun 27 2018, 03:37:03) [MSC v.1900 64 bit (AMD64)] on win32
-#  Type "help", "copyright", "credits" or "license" for more information.
-#  >>> import tst
-#  >>>
-#  >>> dictionary = tst.Checker('words.txt')
-#  >>>
-#  >>> board = tst.Layout(tst.layout1)
-#  >>>
-#  >>> solver = tst.Solver(dictionary, tst.letters1, board)
-#  >>>
-#  >>> solver.print()
-#  3: gel, gen, gnu, gun, leg, lug, peg, pen, pug, pun
-#  4: glen, glue, gulp, leng, luge, lung, plug
-#  5: lunge
-#  6: plunge
-#  >>>
-#  >>> solver.solve()
-#  >>>
-#  >>> for solution in solver.solutions[:3]:
-#  ...     solution.print()
-#  ...
-#  
-#      plunge
-#    g u   u l
-#    lug   l u
-#    u     pun
-#   peg      g
-#     n   l  e
-#    lung u
-#    e  u g
-#    glen e
-#  
-#  Bonus words: gel, gen, pen, leng, plug
-#  
-#      plunge
-#    g u   u l
-#    lug   l u
-#    u     pun
-#   peg      g
-#     n   l  e
-#    lung e
-#    e  u n
-#    glen g
-#  
-#  Bonus words: gel, gen, pen, luge, plug
-#  
-#      plunge
-#    g u   u l
-#    lug   l u
-#    u     pun
-#   peg      g
-#     n   p  e
-#    lung l
-#    e  u u
-#    glen g
-#  
-#  Bonus words: gel, gen, pen, leng, luge
-#  >>>
-#  >>> board[3,7][0][0].set_word('pun')
-#  >>>
-#  >>> board[6,4][1][0].set_word('gun')
-#  >>>
-#  >>> board.set_letter('u', 6, 6)
-#  >>>
-#  >>> solver.solve()
-#  >>>
-#  >>> for solution in solver.solutions:
-#  ...     solution.print()
-#  ...
-#  
-#      plunge
-#    g u   u l
-#    lug   l u
-#    u     pun
-#   peg      g
-#     n   l  e
-#    lung u
-#    e  u g
-#    glen e
-#  
-#  Bonus words: gel, gen, pen, leng, plug
-#  
-#      plunge
-#    g u   u l
-#    lug   l u
-#    u     pun
-#   leg      g
-#     e   l  e
-#    plug u
-#    e  u g
-#    glen e
-#  
-#  Bonus words: gen, gnu, pen, leng, lung
-#  
-#      plunge
-#    g u   u l
-#    lug   l u
-#    u     pun
-#   leg      g
-#     e   l  e
-#    plug u
-#    e  u n
-#    glen g
-#  
-#  Bonus words: gen, gnu, pen, leng, luge
-#  
-#      plunge
-#    g u   u l
-#    leg   l u
-#    u     pun
-#   peg      g
-#     n   l  e
-#    lung u
-#    u  u g
-#    glen e
-#  
-#  Bonus words: gel, gen, pen, leng, plug
-#  
-#      plunge
-#    g e   u l
-#    lug   l u
-#    u     pun
-#   leg      g
-#     e   l  e
-#    plug u
-#    u  u g
-#    glen e
-#  
-#  Bonus words: gen, gnu, pen, leng, lung
-#  
-#      plunge
-#    g e   u l
-#    lug   l u
-#    u     pun
-#   leg      g
-#     e   l  e
-#    plug u
-#    u  u n
-#    glen g
-#  
-#  Bonus words: gen, gnu, pen, leng, luge
-#  >>>
-#  >>> board.set_letter('n', 7, 6)
-#  >>>
-#  >>> board[2,1][0][0].set_word('lug')
-#  >>>
-#  >>> solver.solve()
-#  >>>
-#  >>> for solution in solver.solutions:
-#  ...     solution.print()
-#  ...
-#  
-#      plunge
-#    g u   u l
-#    lug   l u
-#    u     pun
-#   leg      g
-#     e   l  e
-#    plug u
-#    e  u n
-#    glen g
-#  
-#  Bonus words: gen, gnu, pen, leng, luge
-#  
-#      plunge
-#    g e   u l
-#    lug   l u
-#    u     pun
-#   leg      g
-#     e   l  e
-#    plug u
-#    u  u n
-#    glen g
-#  
-#  Bonus words: gen, gnu, pen, leng, luge
-#  >>>
-
-
 #This one has TONS of solutions.  Like thousands.  Because there are so many
 #overlapping 3-letter words
 #556 - Flora -> Seed 12
 #Further investigation has revealed that there are 58296 solutions
+#
 layout0 = [[0,0,0,1,1,1,1,0,0,0,0],
           [0,0,0,0,0,0,1,0,0,0,0],
           [0,0,1,0,1,1,1,1,1,1,0],
@@ -227,38 +48,6 @@ layout3 = [[0,1,1,1,1,0,0],
            [1,0,0,0,0,0,0],
            [1,0,0,0,0,0,0]]
 letters3 = 'candid'
-#
-#1: 206 - Sky -> Dusk 14
-#    PLUNGE
-#  G U   U L
-#  LUG   L U
-#  U     PUN
-# LEG      G
-#   E   L  E
-#  PLUG U
-#  E  U N
-#  GLEN G
-#
-#2: 546 - Flora -> Seed 2
-#   T       C
-#   O     F O
-#   N  I  I I
-#   ICON  CON
-# TIC  FONT
-# O    O  I
-# NIT     O
-#   I I FIN
-#  INTO I
-#     NOT
-#
-#3: 547 - Flora -> Seed 3
-#  ACID
-#  I  I
-# ADD DAD
-#   I  N
-# CANDID
-# A
-# D
 
 
 def fact(n, facts={0:1, 1:1, 2:2}):
@@ -605,7 +394,8 @@ class Layout(object):
             debug: Optional.  Debug flag.  Defaults to False
             filename: Optional.  Filename to write debug logs to.  Defaults to
                       a null string.  If empty, turns off the debug flag
-        Returns:            A list of Solution objects.  Each solution found for the Layout will
+        Returns:
+            A list of Solution objects.  Each solution found for the Layout will
             have a Solution object.
         Raises:
             KeyError: More slots of a specific length exist than words provided
